5ca2883de8ce10c50859bf9420fd312847256d6d
Sergio Matias Piano <smpiano@gmail.com>
2016-11-10 19:08:21 -0300

    correct url

--- app/ui/js/sharedApp.js
+++ app/ui/js/sharedApp.js
@@ -1,5 +1,5 @@
 
-var host = 'http://localhost:5000';
+var host = 'http://jobify-7552-taller2.herokuapp.com:5000';
 var app = angular.module('sharedApp', ['ngMaterial', 'ngRoute']).config(function($mdIconProvider){ 
   $mdIconProvider.icon('menu', './images/menu.svg', 24);});
 
